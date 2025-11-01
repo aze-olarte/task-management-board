1521b3295151e4105919fc4c3fe416110f7b90ea
Azrielle Olarte <azrielleolarte@gmail.com>
2025-11-01 12:43:03 +0800

    feat: add default route with component

---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.scss'
+})
+export class BoardComponent {
+}
