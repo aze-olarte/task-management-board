c59c4e59611cf67119d6db2257ec33066c5023ee
Azrielle Olarte <azrielleolarte@gmail.com>
2025-11-05 04:23:04 +0800

    fix: return msg and add more cases

--- src/app/core/services/interceptor.service.ts
+++ src/app/core/services/interceptor.service.ts
@@ -1,36 +1,39 @@
 import {
-  HttpRequest,
-  HttpHandlerFn,
-  HttpEvent,
-  HttpInterceptorFn,
   HttpErrorResponse,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
 } from '@angular/common/http';
-import { inject } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { NotificationService } from './notification.service';
 
 export const interceptor: HttpInterceptorFn = (
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ): Observable<HttpEvent<any>> => {
-  const notificationService = inject(NotificationService);
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       let msg = '';
       switch (error.status) {
+        case 400:
+          msg = 'Your request could not be processed to due to validation errors.'
+          break;
         case 401:
           msg = 'Unauthorized access.';
+          break;
+        case 403:
+          msg = 'You are not not authorized to perform this action.';
           break;
         case 404:
           msg = 'Requested resource not found.';
           break;
         default:
-          msg = `Error ${error.status}: ${error.message}`;
+          msg = `An unexpected error occurred. Please refresh the page or try again shortly.`;
       }
-      notificationService.showError(msg)
-      return throwError(() => error);
+
+      return throwError(() => msg);
     })
   );
 };
