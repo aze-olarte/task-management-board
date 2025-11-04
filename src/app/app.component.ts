72817aae91b53884d0fd6599ab7cd5ce2511d7d8
Azrielle Olarte <azrielleolarte@gmail.com>
2025-11-05 04:39:28 +0800

    feat: add dashboard component with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutModule],
+  imports: [RouterOutlet, NzLayoutModule, NzMenuModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'task-board',
     loadComponent: () =>
       import('./components/board/board.component').then(
         (m) => m.BoardComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
       ),
   },
 ];
